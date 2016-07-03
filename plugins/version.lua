7d4339bf8449a5253182cfb014fc78a9bbdb6fc5
erf4n <decopaje@yahoo.com>
2016-07-03 00:00:09 -0700

    version

---
+++ plugins/version.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+    return "🤖سلف بوت فارسی توسط 👈@decopaje"
+end 
+return {
+  patterns = {
+    "^(ربات)$",
+    "^bot$",
+    "^سلف بوت$",
+  },
+  run = run
+}
+end
